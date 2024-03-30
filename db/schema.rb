35950f8accc17f911f653f4f4261c45492b67192
= <=>
2024-03-30 12:42:16 -0400

    Change default to field completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_30_162811) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_30_164035) do
   create_table "tasks", force: :cascade do |t|
     t.string "name"
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
