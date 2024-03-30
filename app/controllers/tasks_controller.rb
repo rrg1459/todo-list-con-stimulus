92a94b49ceda3e8cffe6a95b2a306c35f2dcb3c6
= <=>
2024-03-30 13:02:44 -0400

    Add broadcasts to controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,9 @@ class TasksController < ApplicationController
 
   # GET /tasks or /tasks.json
   def index
-    @tasks = Task.all
+    @tasks = Task.where(completed: false)
+    @done_tasks = Task.where(completed: true)
+    @task = Task.new
   end
 
   # GET /tasks/1 or /tasks/1.json
@@ -19,14 +21,25 @@ class TasksController < ApplicationController
   def edit
   end
 
+
+  def active
+    @task = Task.find(params[:id])
+    @task.update(completed: !@task.completed)
+    respond_to do |format|
+      format.turbo_stream { render partial: 'tasks/active', locals: { task: @task } }
+      @task.broadcast_render_later_to "tasks", partial: "tasks/active", locals: { task: @task }
+    end
+  end
+
   # POST /tasks or /tasks.json
   def create
     @task = Task.new(task_params)
 
     respond_to do |format|
       if @task.save
-        format.html { redirect_to task_url(@task), notice: "Task was successfully created." }
+        format.html {}
         format.json { render :show, status: :created, location: @task }
+        @task.broadcast_render_to "tasks", partial: "tasks/create", locals: { task: @task }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @task.errors, status: :unprocessable_entity }
